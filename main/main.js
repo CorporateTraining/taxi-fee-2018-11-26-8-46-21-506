043c7d4db887bd91bb957288ceb80dac6e0dc4c1
mahuanfy <mahuanfy@foxmail.com>
2019-02-14 14:13:19 +0800

    [HuanHuan]: Refactoring test

--- main/main.js
+++ main/main.js
@@ -1,11 +1,12 @@
 module.exports = function main(input) {
     let price = 6
-    if(input.distance <= 2){
-        price += input.wait * 0.25 
-    }else if(input.distance > 2 && input.distance <= 8){
-        price += ((input.distance - 2) * 0.8 + input.wait * 0.25)
+    
+    if(input.distance > 2 && input.distance <= 8){
+        price += (input.distance - 2) * 0.8
     }else if(input.distance > 8){
-        price += ((input.distance - 2) * 0.8 +(input.distance - 8) * 0.4+ input.wait * 0.25)
+        price += (input.distance - 2) * 0.8 + (input.distance - 8) * 0.4
     }
+    price += input.wait * 0.25
+
     return Math.round(price);
 };
