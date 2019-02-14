c35011db3eeb945cf69578999595ea94174941f2
mahuanfy <mahuanfy@foxmail.com>
2019-02-14 13:55:21 +0800

    [HuanHuan]: Test taxi driving 2 km

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    let price = 6
+    if(input.distance <= 2){
+        price += input.wait * 0.25 
+    }
+    return Math.round(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,16 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it("taxi driving 2 km no waiting ", function(){
+        var input = {distance: 2, wait: 0}
+        var result = main(input);
+        var expect_value = 6;
+        expect(expect_value).toEqual(result)
+    })
+    it("taxi driving 2 km and waiting 5 min ", function(){
+        var input = {distance: 2, wait: 5}
+        var result = main(input);
+        var expect_value = 7;
+        expect(expect_value).toEqual(result)
+    })
 });
