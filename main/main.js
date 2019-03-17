f0ff7132618a6cdb6a5b15dae72165f9aa7c031d
mahuanfy <mahuanfy@foxmail.com>
2019-03-17 13:51:51 +0800

    [HuanHuan]: Delete no user test and add semicolon

--- main/main.js
+++ main/main.js
@@ -1,12 +1,12 @@
 module.exports = function main(input) {
-    let price = 6
-    
+    let price = 6;
+
     if(input.distance > 2 && input.distance <= 8){
-        price += (input.distance - 2) * 0.8
+        price += (input.distance - 2) * 0.8;
     }else if(input.distance > 8){
-        price += (input.distance - 2) * 0.8 + (input.distance - 8) * 0.4
+        price += (input.distance - 2) * 0.8 + (input.distance - 8) * 0.4;
     }
-    price += input.wait * 0.25
+    price += input.wait * 0.25;
 
     return Math.round(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,45 +2,39 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     it("taxi driving 2 km no waiting ", function(){
-        var input = {distance: 2, wait: 0}
+        var input = {distance: 2, wait: 0};
         var result = main(input);
         var expect_value = 6;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
     it("taxi driving 2 km and waiting 5 min ", function(){
-        var input = {distance: 2, wait: 5}
+        var input = {distance: 2, wait: 5};
         var result = main(input);
         var expect_value = 7;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
     it("taxi driving 5 km and no waiting", function(){
-        var input = {distance: 5, wait: 0}
+        var input = {distance: 5, wait: 0};
         var result = main(input);
         var expect_value = 8;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
     it("taxi driving 5 km and waiting 5 min ", function(){
-        var input = {distance: 5, wait: 5}
+        var input = {distance: 5, wait: 5};
         var result = main(input);
         var expect_value = 10;
-        expect(expect_value).toEqual(result)
-    })
-    it("taxi driving 8 km and waiting 5 min ", function(){
-        var input = {distance: 8, wait: 5}
-        var result = main(input);
-        var expect_value = 12;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
     it("taxi driving 10 km and waiting 0 min ", function(){
-        var input = {distance: 10, wait: 0}
+        var input = {distance: 10, wait: 0};
         var result = main(input);
         var expect_value = 13;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
     it("taxi driving 10 km and waiting 5 min ", function(){
-        var input = {distance: 10, wait: 5}
+        var input = {distance: 10, wait: 5};
         var result = main(input);
         var expect_value = 14;
-        expect(expect_value).toEqual(result)
-    })
+        expect(expect_value).toEqual(result);
+    });
 });
